3a900667c53747d7a6a9e463186118df5035a008
Braian Corrêa <00.braian.dev@gmail.com>
2021-05-23 13:55:18 -0300

    Start Project

---
+++ components/auth/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+
+export class Landing extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text> Papai Decinho Arrasa!</Text>
+      </View>
+    );
+  }
+}
+
+export default Landing;
